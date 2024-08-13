6e6b91caa03417dfba8b6eebe9346d294bf4df1c
Ikhsan Ainul <136774628+Ikhsan012@users.noreply.github.com>
2024-08-13 15:24:36 +0800

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://i1.sndcdn.com/avatars-000214125831-5q6tdw-t500x500.jpg">
+        class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://i.ibb.co.com/k55jFR3/331834353-226610709813393-3086375254859984391-n.jpg">
     </div>
   </main>
 </template>
